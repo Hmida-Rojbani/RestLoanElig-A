7c60a4c473f515a0956bbcecdd324639a34def56
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-04-26 13:25:18 +0100

    add spring doc

--- src/main/java/de/tekup/rest/RestLoanEligAApplication.java
+++ src/main/java/de/tekup/rest/RestLoanEligAApplication.java
@@ -3,7 +3,14 @@ package de.tekup.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(version = "1.0",
+title = "LoanIndicatorService", description = "Check clients for banking services",
+contact = @Contact(name = "Hmida Rojbani",email = "hmida.rojbani@tek-up.tn")))
 public class RestLoanEligAApplication {
 
 	public static void main(String[] args) {
--- src/main/java/de/tekup/rest/ctrls/LaonCtrl.java
+++ src/main/java/de/tekup/rest/ctrls/LaonCtrl.java
@@ -1,13 +1,20 @@
 package de.tekup.rest.ctrls;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.tekup.rest.models.CustomerRequest;
 import de.tekup.rest.models.WsResponse;
 import de.tekup.rest.services.LoanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -16,11 +23,29 @@ public class LaonCtrl {
 	
 	private LoanService service;
 	
+	@Operation(description = "Check client status for a credit.",operationId = "checkClient"
+			,responses = {
+					@ApiResponse(responseCode = "200", description = "Return Status"),
+					@ApiResponse(responseCode = "400", description = "Not yet Implemented")
+			})
 	@PostMapping(path="/check/client/status",
 			consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE},
 			produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
 	public WsResponse getStatus(@RequestBody CustomerRequest request) {
 		return service.getLoanStatus(request);
+	}
+	
+	@GetMapping("/test/service")
+	public String ping() {
+		return "hello";
+	}
+	@PutMapping("/test/service")
+	public String ping1() {
+		return "hello";
+	}
+	@DeleteMapping("/test/service")
+	public String ping2() {
+		return "hello";
 	}
 
 }
