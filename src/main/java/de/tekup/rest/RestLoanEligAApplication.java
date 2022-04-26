fe09e4980b47233532dff9ceceecd78e77e2dfad
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-04-26 13:01:13 +0100

    REST API

---
+++ src/main/java/de/tekup/rest/RestLoanEligAApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLoanEligAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestLoanEligAApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/tekup/rest/services/LoanService.java
@@ -0,0 +1,34 @@
+package de.tekup.rest.services;
+
+import org.springframework.stereotype.Service;
+
+import de.tekup.rest.models.CustomerRequest;
+import de.tekup.rest.models.WsResponse;
+
+@Service
+public class LoanService {
+
+	public WsResponse getLoanStatus(CustomerRequest request) {
+		WsResponse response = new WsResponse();
+		if (request.getAge() < 25 || request.getAge() > 50) {
+			response.getCriteriaMismatch().add("Customer age must be between 25 and 50.");
+		}
+		if (request.getYearlyIncome() < 25) {
+			response.getCriteriaMismatch().add("Customer age must be between 25 and 50.");
+		}
+		// cibile score 1 -> 999
+		if (request.getCibilScore() < 500) {
+			response.getCriteriaMismatch().add("Customer cibile score must be over 500.");
+		}
+		if (response.getCriteriaMismatch().isEmpty()) {
+			response.setEligible(true);
+			response.setApprovedAmount((long) ((50 - request.getAge()) * request.getYearlyIncome() * 0.4));
+		} else {
+			response.setEligible(false);
+			response.setApprovedAmount(0);
+		}
+
+		return response;
+	}
+
+}
